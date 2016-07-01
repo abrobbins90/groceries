9f7d34b2d8700ff4d51e4a2f3f3cfa922f939a0d
Matthew Lancellotti <mareoraft@gmail.com>
2016-07-01 09:32:09 -0400

    fixed syntax errors in tornado/main.py

--- tornado/main.py
+++ tornado/main.py
@@ -11,7 +11,7 @@ from tornado.log import enable_pretty_logging
 class BaseHandler (RequestHandler):
 	def initialize(self, var):
 		#init stuff!
-		print( var )
+		print var
 		pass
 	def get(self, num):
 		self.write("this is "+num+"!")
@@ -26,37 +26,37 @@ class FormHandler (BaseHandler):
 					""")
 	def post(self):
 		invar = self.get_body_argument("thisistheonlyinput")
-		print( "going to post the input!"+invar )
+		print "going to post the input!" + invar
 
 # The WebSocket protocol is still in development. This module currently implements the hixie-76 and hybi-10 versions of the protocol. See this browser compatibility table on Wikipedia: http://en.wikipedia.org/wiki/WebSockets#Browser_support
 class SocketHandler (WebSocketHandler):
-        def open(self):
-                print('websocket opened!')
+		def open(self):
+			print 'websocket opened!'
 
-        def on_message(self, message):
-                print('got message: ' + message)
-                
-		# Check to ensure a command is received
-		if "command" not in message:
-			raise KeyError("Key 'command' is missing. Way to suck...")
-		# Check the command received and proceed accordingly
-		if message["command"] == "add meal":
-			print 'add me'
-		elif message["command"] == "edit meal":
-			print 'edit me'
-		elif message["command"] == "remove meal":
-			print 'remove me'
-                # send a message back
-                self.write_message('ten four')
+		def on_message(self, message):
+			print 'got message: ' + message
 
-        def on_close(self):
-                print('websocket closed')
+			# Check to ensure a command is received
+			if "command" not in message:
+				raise KeyError("Key 'command' is missing. Way to suck...")
+			# Check the command received and proceed accordingly
+			if message["command"] == "add meal":
+				print 'add me'
+			elif message["command"] == "edit meal":
+				print 'edit me'
+			elif message["command"] == "remove meal":
+				print 'remove me'
+				# send a message back
+				self.write_message('ten four')
+
+		def on_close(self):
+			print 'websocket closed'
 
 def make_app():
 	return Application(
 		[
-                        url('/mySocket', SocketHandler, {} , name = "mySocket!!!"),
-                   	url('/form', FormHandler, { "var":"initialize this!" }, name = "forlorn"),
+			url('/mySocket', SocketHandler, {} , name = "mySocket!!!"),
+			url('/form', FormHandler, { "var":"initialize this!" }, name = "forlorn"),
 			url(r'/(.*)', StaticFileHandler, { "path": "../www-built/" }) # captures anything at all, and serves it as a static file. simple!
 		],
 		#settings
@@ -71,4 +71,5 @@ def main():
 	#other stuff
 	IOLoop.current().start()
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+	main()
